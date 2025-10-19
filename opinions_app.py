f4377ba6301724f9447bbfdcdc97495e57dfdfe4
sheferino <lmahaon@yandex.ru>
2025-10-19 15:02:14 +0300

    Add sqlalchemy

--- opinions_app.py
+++ opinions_app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# этот ключ flask_alchemy использует автоматом
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def index_view():
+    print(app.config)
     return 'Совсем скоро тут будет случайное мнение о фильме!'
 
 if __name__ == '__main__':
